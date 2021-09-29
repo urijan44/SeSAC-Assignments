6294ada55559f8d2327d6f621ae13908aefd9771
Henry Lee <46310414+urijan44@users.noreply.github.com>
2021-09-29 18:04:48 +0900

    Initial Commit

---
+++ NetflixCloneCoding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixCloneCoding
+//
+//  Created by hoseung Lee on 2021/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
