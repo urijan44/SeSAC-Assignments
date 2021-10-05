8077e3fa70f8beffb72670a59b2aa737c41fbb49
Henry Lee <46310414+urijan44@users.noreply.github.com>
2021-10-05 10:05:10 +0900

    Initial Commit

---
+++ EmotionDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmotionDiary
+//
+//  Created by hoseung Lee on 2021/10/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
